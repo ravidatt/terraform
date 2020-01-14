1040a64ebc7a80208acc4ffb20c7c427aee4c5b8
EC2 Default User <ec2-user@ip-172-31-86-206.ec2.internal>
2020-01-14 10:11:18 +0000

    lab test

---
+++ AWS/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+
+# Deploy Storage Resource
+module "storage" {
+  source       = "./storage"
+  project_name = "${var.project_name}"
+}
+
+# Deploy Networking Resources
+
+module "networking" {
+  source       = "./networking"
+  vpc_cidr     = "${var.vpc_cidr}"
+  public_cidrs = "${var.public_cidrs}"
+  accessip     = "${var.accessip}"
+}
---
+++ AWS/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {}
+
+#------ storage variables
+
+variable "project_name" {}
+
+#-------networking variables
+
+variable "vpc_cidr" {}
+
+variable "public_cidrs" {
+  type = "list"
+}
+
+variable "accessip" {}
